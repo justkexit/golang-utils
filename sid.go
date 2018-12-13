eac77266220099e6561483b06b243030ef413fa2
Alexander Vyrov <42142193+justkexit@users.noreply.github.com>
2018-12-13 19:37:03 +0400

    Create sid.go

---
+++ sid.go
@@ -0,0 +1,21 @@
+package golang-utils
+
+import (
+	"encoding/binary"
+	"fmt"
+	"strings"
+)
+
+// StringSID converts bytes Windows SID to string representation
+func StringSID(sid []byte) string {
+	var par = []string{"S"}
+	par = append(par, fmt.Sprintf("%x", uint(sid[0])))
+	cnt := int(sid[1])
+	par = append(par, fmt.Sprintf("%d", binary.BigEndian.Uint64(append([]byte{0, 0}, sid[2:8]...))))
+	var pos = 8
+	for i := 0; i < cnt; i++ {
+		par = append(par, fmt.Sprintf("%d", binary.LittleEndian.Uint32(sid[pos:pos+4])))
+		pos += 4
+	}
+	return strings.Join(par, "-")
+}
